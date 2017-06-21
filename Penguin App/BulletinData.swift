db512280443e83768caaf63a794fe4b97f83aea5
jacob <jabrah29@uic.edu>
2017-06-21 02:22:54 -0400

    v

---
+++ Penguin App/BulletinData.swift
@@ -0,0 +1,26 @@
+//
+//  BulletinData.swift
+//  Penguin App
+//
+//  Created by Jacob Abraham on 6/19/17.
+//  Copyright © 2017 Jacob Abraham. All rights reserved.
+//
+
+import Foundation
+
+class BulletinData{
+    
+     let headline:String
+     let details:String
+     let date:String
+     let time:String
+    
+    
+    
+    init(headline:String, details:String,date:String, time:String) {
+        self.headline=headline
+        self.details=details
+        self.date=date
+        self.time=time
+    }
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '9.0'
+
+target 'Penguin App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Penguin App
+  pod 'Firebase/Storage'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  target 'Penguin AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
